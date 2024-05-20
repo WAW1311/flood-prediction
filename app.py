d01e562d6c8d16d20fc9077fd78cc48eb5a5a631
WAW1311 <wahyuandrewibowo311@gmail.com>
2024-05-21 04:17:20 +0700

    add kebakaran lahan dan erupsi

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify,request
 from model import prediksibanjir
+from model import lahan
+from model import erupsi
 
 app = Flask(__name__)
 
 
 
-@app.route('/prediction', methods=['POST'])
-def prediction():
+@app.route('/banjir-prediction', methods=['POST'])
+def prediction1():
     input = request.json
     if input is None:
         return jsonify({
@@ -14,6 +16,40 @@ def prediction():
             'eror':'Prediksi tidak ditemukan!'
             }),400
     prediksi = prediksibanjir.prediksi(input)
+    result = prediksi.tolist()
+    data = {
+        'status': 200,
+        'msg':'berhasil melakukan prediksi!',
+        'prediksi': result,
+    }
+    return jsonify(data),200
+
+@app.route('/erupsi-prediction', methods=['POST'])
+def prediction2():
+    input = request.json
+    if input is None:
+        return jsonify({
+            'status': 400,
+            'eror':'Prediksi tidak ditemukan!'
+            }),400
+    prediksi = erupsi.prediksi(input)
+    result = prediksi.tolist()
+    data = {
+        'status': 200,
+        'msg':'berhasil melakukan prediksi!',
+        'prediksi': result,
+    }
+    return jsonify(data),200
+
+@app.route('/kebakaran-prediction', methods=['POST'])
+def prediction3():
+    input = request.json
+    if input is None:
+        return jsonify({
+            'status': 400,
+            'eror':'Prediksi tidak ditemukan!'
+            }),400
+    prediksi = lahan.prediksi(input)
     result = prediksi.tolist()
     data = {
         'status': 200,
